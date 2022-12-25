39d328cc207b5ad2be8fcfabf9a42758023533f9
cuervolu <cuervolu@protonmail.com>
2022-12-25 20:11:19 -0300

    chore: eliminación de importación no usada

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { JournalApp } from "./JournalApp";
 import { store } from "./store";
